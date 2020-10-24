33cc3477780ea410f2121ab3606230abe622c9c3
unknown <maged.rajab@hotmail.com>
2020-10-24 10:34:27 +0300

    validate charts

--- resources/views/charts.blade.php
+++ resources/views/charts.blade.php
@@ -3,18 +3,10 @@
 @section('content')
     <div id="chart_div"></div>
     <script>
-        // Load the Visualization API and the corechart package.
         google.charts.load('current', {'packages':['corechart']});
-
-        // Set a callback to run when the Google Visualization API is loaded.
         google.charts.setOnLoadCallback(drawChart);
-
-        // Callback that creates and populates a data table,
-        // instantiates the pie chart, passes in the data and
-        // draws it.
         function drawChart() {
 
-        // Create the data table.
         var data = new google.visualization.DataTable();
         data.addColumn('string', 'do');
         data.addColumn('number', 'per');
@@ -24,12 +16,10 @@
             @endforeach
         ]);
 
-        // Set chart options
         var options = {'title':'How Much Pizza I Ate Last Night',
                         'width':400,
                         'height':300};
 
-        // Instantiate and draw our chart, passing in some options.
         var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
         chart.draw(data, options);
         }
